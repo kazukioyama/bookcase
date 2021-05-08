cdb9149686818ed9409adef0d0c3b320f83642db
大山一毅 <oyamakazuki@ooyamahajimetakeshinoMacBook-Pro.local>
2021-05-08 19:37:09 +0900

    ステップ1：書籍のリストを表示する

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
+/* eslint-disable */
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import { BookToRead } from "./BookToRead";
+import BookRow from "./BookRow";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const dummyBooks: BookToRead[] = [
+    {
+        id: 1,
+        title: "はじめてのReact",
+        authors: "ダミー",
+        memo: ""
+    },
+    {
+        id: 2,
+        title: "React Hooks入門",
+        authors: "ダミー",
+        memo: ""
+    },
+    {
+        id: 3,
+        title: "実践Reactアプリケーション開発",
+        authors: "ダミー",
+        memo: ""
+    }
+];
+
+const App = () => {
+    const [books, setBooks] = useState(dummyBooks);
+
+    const bookRows = books.map((b) => {
+        return (
+        <BookRow
+            book={b}
+            key={b.id}
+            onMemoChange={(id, memo) => {}}
+            onDelete={(id) => {}}
+        />
+        );
+    });
+
+    return (
+        <div className="App">
+          <section className="nav">
+            <h1>読みたい本リスト</h1>
+            <div className="button-like">本を追加</div>
+          </section>
+          <section className="main">{bookRows}</section>
+        </div>
+    );
+
+};
 
 export default App;
